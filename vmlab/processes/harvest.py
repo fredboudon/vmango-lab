8781d2eda115b007b5bce2a16c4f467b52f359c6
jvail <6503378+jvail@users.noreply.github.com>
2021-03-31 13:17:44 +0200

    wip

--- vmlab/processes/harvest.py
+++ vmlab/processes/harvest.py
@@ -25,11 +25,24 @@ class Harvest(ParameterizedProcess):
         global_name='ripeness_index'
     )
 
+    nb_fruit_harvested = xs.variable(
+        dims=('GU'),
+        intent='inout',
+        description='ripening index',
+        attrs={
+            'unit': '-'
+        },
+        global_name=''
+    )
+
     def initialize(self):
         super(Harvest, self).initialize()
         self.ripeness_index = np.zeros(self.nb_gu, dtype=np.float32)
+        self.nb_fruit_harvested = np.zeros(self.nb_gu, dtype=np.float32)
 
     @xs.runtime(args=())
     def run_step(self):
         Tthresh_fruit_ripe = self.parameters.Tthresh_fruit_ripe
         self.ripeness_index[self.fruit_growth_tts > 0.] = np.minimum(1.0, self.fruit_growth_tts[self.fruit_growth_tts > 0.] / Tthresh_fruit_ripe)
+        harvest = (self.nb_fruit_harvested == 0) & (self.ripeness_index == 1.)
+        self.nb_fruit_harvested[harvest] = self.ripeness_index[harvest]
