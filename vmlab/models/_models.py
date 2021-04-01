0f198069ff5eef6a92d5e6b19032fd0fdb76283b
jvail <6503378+jvail@users.noreply.github.com>
2021-04-01 12:16:38 +0400

    wip

---
+++ vmlab/models/_models.py
@@ -0,0 +1,77 @@
+import xsimlab as xs
+
+from ..processes import (
+    topology,
+    geometry,
+    environment,
+    phenology,
+    appearance,
+    growth,
+    arch_dev_veg_within,
+    arch_dev_veg_between,
+    arch_dev_rep,
+    arch_dev_mix,
+    arch_dev,
+    light_interception,
+    photosynthesis,
+    carbon_allocation,
+    carbon_reserve,
+    carbon_demand,
+    carbon_balance,
+    # fruit_quality,
+    harvest
+)
+
+
+vmango = xs.Model({
+    'environment': environment.Environment,
+    'phenology': phenology.Phenology,
+    'topology': topology.Topology,
+    'geometry': geometry.Geometry,
+    'appearance': appearance.Appearance,
+    'growth': growth.Growth,
+    'arch_dev_veg_within': arch_dev_veg_within.ArchDevVegWithin,
+    'arch_dev_veg_between': arch_dev_veg_between.ArchDevVegBetween,
+    'arch_dev_rep': arch_dev_rep.ArchDevRep,
+    'arch_dev_mix': arch_dev_mix.ArchDevMix,
+    'arch_dev': arch_dev.ArchDev,
+    'light_interception': light_interception.LightInterception,
+    'photosynthesis': photosynthesis.Photosythesis,
+    'carbon_allocation': carbon_allocation.CarbonAllocation,
+    'carbon_reserve': carbon_reserve.CarbonReserve,
+    'carbon_demand': carbon_demand.CarbonDemand,
+    'carbon_balance': carbon_balance.CarbonBalance,
+    # 'fruit_quality': fruit_quality.FruitQuality,
+    'harvest': harvest.Harvest
+})
+
+arch_dev_model = xs.Model({
+    'topology': topology.Topology,
+    'arch_dev_veg_within': arch_dev_veg_within.ArchDevVegWithin,
+    'arch_dev_veg_between': arch_dev_veg_between.ArchDevVegBetween,
+    'arch_dev_rep': arch_dev_rep.ArchDevRep,
+    'arch_dev_mix': arch_dev_mix.ArchDevMix,
+    'arch_dev': arch_dev.ArchDev
+})
+
+fruit_model = xs.Model({
+    'environment': environment.Environment,
+    'phenology': phenology.Phenology,
+    'topology': topology.Topology,
+    'geometry': geometry.Geometry,
+    'appearance': appearance.Appearance,
+    'growth': growth.Growth,
+    'arch_dev_veg_within': arch_dev_veg_within.ArchDevVegWithin,
+    'arch_dev_veg_between': arch_dev_veg_between.ArchDevVegBetween,
+    'arch_dev_rep': arch_dev_rep.ArchDevRep,
+    'arch_dev_mix': arch_dev_mix.ArchDevMix,
+    'arch_dev': arch_dev.ArchDev,
+    'light_interception': light_interception.LightInterception,
+    'photosynthesis': photosynthesis.Photosythesis,
+    'carbon_allocation': carbon_allocation.CarbonAllocation,
+    'carbon_reserve': carbon_reserve.CarbonReserve,
+    'carbon_demand': carbon_demand.CarbonDemand,
+    'carbon_balance': carbon_balance.CarbonBalance,
+    # 'fruit_quality': fruit_quality.FruitQuality,
+    'harvest': harvest.Harvest
+})
