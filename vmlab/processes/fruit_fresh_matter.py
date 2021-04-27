965025fb648f76f936ebea32f6146411391df653
igrechi <52966941+igrechi@users.noreply.github.com>
2021-04-27 15:01:35 +0400

    wip

--- vmlab/processes/fruit_fresh_matter.py
+++ vmlab/processes/fruit_fresh_matter.py
@@ -2,15 +2,15 @@ import xsimlab as xs
 import numpy as np
 
 from vmlab.constants import (
-    R, density_W, MM_water, MM_mal, MM_cit, MM_pyr, MM_oxa, MM_K,
-    MM_Mg, MM_Ca, MM_NH4, MM_Na, MM_glc, MM_frc, MM_suc
+    R, density_W, MM_water
 )
 
 from . import (
     environment,
     carbon_demand,
     carbon_balance,
-    phenology
+    phenology,
+    fruit_quality
 )
 from ._base.parameter import ParameterizedProcess
 
@@ -18,7 +18,7 @@ from ._base.parameter import ParameterizedProcess
 @xs.process
 class FruitFreshMatter(ParameterizedProcess):
     """
-    Process computing fresh matter changes
+    Process computing fresh matter and quality (sugars and acids content) changes
     """
     nb_gu = xs.global_ref('nb_gu')
 
@@ -26,15 +26,20 @@ class FruitFreshMatter(ParameterizedProcess):
     GR_day = xs.foreign(environment.Environment, 'GR_day')
     RH_day = xs.foreign(environment.Environment, 'RH_day')
 
+    nmol_solutes = xs.foreign(fruit_quality.FruitQuality, 'nmol_solutes')
+    mass_suc = xs.foreign(fruit_quality.FruitQuality, 'mass_suc')
+    mass_glc = xs.foreign(fruit_quality.FruitQuality, 'mass_glc')
+    mass_frc = xs.foreign(fruit_quality.FruitQuality, 'mass_frc')
+    mass_sta = xs.foreign(fruit_quality.FruitQuality, 'mass_sta')
+    mass_mal = xs.foreign(fruit_quality.FruitQuality, 'mass_mal')
+    mass_cit = xs.foreign(fruit_quality.FruitQuality, 'mass_cit')
+
     fruit_growth_tts = xs.foreign(phenology.Phenology, 'fruit_growth_tts')
     nb_fruit = xs.foreign(phenology.Phenology, 'nb_fruit')
     fruited = xs.foreign(phenology.Phenology, 'fruited')
 
     DM_fruit_delta = xs.foreign(carbon_balance.CarbonBalance, 'DM_fruit_delta')
     DM_fruit = xs.foreign(carbon_balance.CarbonBalance, 'DM_fruit')
-    # this dummy just forces process ordering
-    __dummy = xs.foreign(carbon_balance.CarbonBalance, 'carbon_supply')
-
     DM_fruit_0 = xs.foreign(carbon_demand.CarbonDemand, 'DM_fruit_0')
 
     FM_fruit = xs.variable(
@@ -134,7 +139,7 @@ class FruitFreshMatter(ParameterizedProcess):
         attrs={
             'unit': 'g g-1 FM'
         },
-        groups='fruit_quality'
+        groups='fruit_fresh_matter'
     )
 
     glucose = xs.variable(
@@ -238,21 +243,7 @@ class FruitFreshMatter(ParameterizedProcess):
             swp_4 = params.swp_4
             ddthres_1 = params.ddthres_1
             ddthres_2 = params.ddthres_2
-            delta_mal = params.delta.mal
-            delta_cit = params.delta.cit
-            delta_pyr = params.delta.pyr
-            delta_oxa = params.delta.oxa
-            delta_K = params.delta.K
-            delta_Mg = params.delta.Mg
-            delta_Ca = params.delta.Ca
-            delta_NH4 = params.delta.NH4
-            delta_Na = params.delta.Na
-            delta_glc = params.delta.glc
-            delta_frc = params.delta.frc
-            delta_suc = params.delta.suc
-            delta_sta = params.delta.sta
             dd_thresh = params.dd_thresh
-            Tthresh_fruit_stage = params.Tthresh_fruit_stage
 
             e_fruitDM2FM_1 = params.e_fruitDM2FM_1
             e_fruitDM2FM_2 = params.e_fruitDM2FM_2
@@ -283,64 +274,20 @@ class FruitFreshMatter(ParameterizedProcess):
             DM_fleshpeel_delta = (e_fruit2fleshDM_1 * e_fruit2fleshDM_2 * self.DM_fruit[fruiting] ** (e_fruit2fleshDM_2 - 1) + e_fruit2peelDM_1 * e_fruit2peelDM_2 *
                                   self.DM_fruit[fruiting] ** (e_fruit2peelDM_2 - 1)) * self.DM_fruit_delta[fruiting]
             DM_fleshpeel_delta = np.maximum(0, DM_fleshpeel_delta)
-            DM_flesh_previous = e_fleshpeel2fleshDM * DM_fleshpeel_previous
             W_flesh_previous = e_fleshpeel2fleshW * self.W_fleshpeel[fruiting]
 
             # ========================================================================================================================
             # OSMOTIC PRESSURE IN THE FRUIT
             # ========================================================================================================================
-
-            # -- mass proportion of osmotically active solutes & starch in the dry mass of fruit flesh (eq.9) :
-            fruit_growth_tts_delta = self.fruit_growth_tts[fruiting] - Tthresh_fruit_stage
-            DM_flesh_x_tts_delta = DM_flesh_previous * fruit_growth_tts_delta
-            prop_mal = np.maximum(0, delta_mal[0] + delta_mal[1] * fruit_growth_tts_delta + delta_mal[2] * DM_flesh_previous + delta_mal[3] * DM_flesh_x_tts_delta)
-            prop_cit = np.maximum(0, delta_cit[0] + delta_cit[1] * fruit_growth_tts_delta + delta_cit[2] * DM_flesh_previous + delta_cit[3] * DM_flesh_x_tts_delta)
-            prop_pyr = np.maximum(0, delta_pyr[0] + delta_pyr[1] * fruit_growth_tts_delta + delta_pyr[2] * DM_flesh_previous + delta_pyr[3] * DM_flesh_x_tts_delta)
-            prop_oxa = np.maximum(0, delta_oxa[0] + delta_oxa[1] * fruit_growth_tts_delta + delta_oxa[2] * DM_flesh_previous + delta_oxa[3] * DM_flesh_x_tts_delta)
-            prop_K = np.maximum(0, delta_K[0] + delta_K[1] * fruit_growth_tts_delta + delta_K[2] * DM_flesh_previous + delta_K[3] * DM_flesh_x_tts_delta)
-            prop_Mg = np.maximum(0, delta_Mg[0] + delta_Mg[1] * fruit_growth_tts_delta + delta_Mg[2] * DM_flesh_previous + delta_Mg[3] * DM_flesh_x_tts_delta)
-            prop_Ca = np.maximum(0, delta_Ca[0] + delta_Ca[1] * fruit_growth_tts_delta + delta_Ca[2] * DM_flesh_previous + delta_Ca[3] * DM_flesh_x_tts_delta)
-            prop_NH4 = np.maximum(0, delta_NH4[0] + delta_NH4[1] * fruit_growth_tts_delta + delta_NH4[2] * DM_flesh_previous + delta_NH4[3] * DM_flesh_x_tts_delta)
-            prop_Na = np.maximum(0, delta_Na[0] + delta_Na[1] * fruit_growth_tts_delta + delta_Na[2] * DM_flesh_previous + delta_Na[3] * DM_flesh_x_tts_delta)
-            prop_glc = np.maximum(0, delta_glc[0] + delta_glc[1] * fruit_growth_tts_delta + delta_glc[2] * DM_flesh_previous + delta_glc[3] * DM_flesh_x_tts_delta)
-            prop_frc = np.maximum(0, delta_frc[0] + delta_frc[1] * fruit_growth_tts_delta + delta_frc[2] * DM_flesh_previous + delta_frc[3] * DM_flesh_x_tts_delta)
-            prop_suc = np.maximum(0, delta_suc[0] + delta_suc[1] * fruit_growth_tts_delta + delta_suc[2] * DM_flesh_previous + delta_suc[3] * DM_flesh_x_tts_delta)
-            prop_sta = np.maximum(0, delta_sta[0] + delta_sta[1] * fruit_growth_tts_delta + delta_sta[2] * DM_flesh_previous + delta_sta[3] * DM_flesh_x_tts_delta)
-
-            # -- mass and number of moles of osmotically active solutes & starch in fruit flesh (eq.8) :
-            mass_mal = prop_mal * DM_flesh_previous
-            nmol_mal = mass_mal / MM_mal
-            mass_cit = prop_cit * DM_flesh_previous
-            nmol_cit = mass_cit / MM_cit
-            mass_pyr = prop_pyr * DM_flesh_previous
-            nmol_pyr = mass_pyr / MM_pyr
-            mass_oxa = prop_oxa * DM_flesh_previous
-            nmol_oxa = mass_oxa / MM_oxa
-            mass_K = prop_K * DM_flesh_previous
-            nmol_K = mass_K / MM_K
-            mass_Mg = prop_Mg * DM_flesh_previous
-            nmol_Mg = mass_Mg / MM_Mg
-            mass_Ca = prop_Ca * DM_flesh_previous
-            nmol_Ca = mass_Ca / MM_Ca
-            mass_NH4 = prop_NH4 * DM_flesh_previous
-            nmol_NH4 = mass_NH4 / MM_NH4
-            mass_Na = prop_Na * DM_flesh_previous
-            nmol_Na = mass_Na / MM_Na
-            mass_glc = prop_glc * DM_flesh_previous
-            nmol_glc = mass_glc / MM_glc
-            mass_frc = prop_frc * DM_flesh_previous
-            nmol_frc = mass_frc / MM_frc
-            mass_suc = prop_suc * DM_flesh_previous
-            nmol_suc = mass_suc / MM_suc
-            mass_sta = prop_sta * DM_flesh_previous
+            # from fruit growth model in Léchaudel et al (2007)
 
             # -- osmotic pressure in fruit flesh (eq.6-7) :
-            nmol_solutes = nmol_mal + nmol_cit + nmol_pyr + nmol_oxa + nmol_K + nmol_Mg + nmol_Ca + nmol_NH4 + nmol_Na + nmol_glc + nmol_frc + nmol_suc
-            self.osmotic_pressure_fruit[fruiting] = (R * (self.TM_day + 273.15) * nmol_solutes) / (W_flesh_previous / density_W) + osmotic_pressure_aa
+            self.osmotic_pressure_fruit[fruiting] = (R * (self.TM_day + 273.15) * self.nmol_solutes[fruiting]) / (W_flesh_previous / density_W) + osmotic_pressure_aa
 
             # ========================================================================================================================
             # FRUIT TRANSPIRATION
             # ========================================================================================================================
+            # from fruit growth model in Léchaudel et al (2007)
 
             # -- fruit surface (eq.3) :
             A_fruit = e_fruitFM2surface_1 * self.FM_fruit[fruiting] ** e_fruitFM2surface_2
@@ -356,6 +303,7 @@ class FruitFreshMatter(ParameterizedProcess):
             # ========================================================================================================================
             # CELL WALL PROPERTIES OF THE FRUIT
             # ========================================================================================================================
+            # from fruit growth model in Léchaudel et al (2007)
 
             # -- cell wall extensibility (eq.18) :
             if np.isnan(dd_thresh):                                                                                                       # _MODIF 2017-05_1
@@ -371,6 +319,7 @@ class FruitFreshMatter(ParameterizedProcess):
             # ========================================================================================================================
             # TURGOR PRESSURE & WATER POTENTIAL IN THE FRUIT
             # ========================================================================================================================
+            # from fruit growth model in Léchaudel et al (2007)
 
             # -- water potential of the stem :
             water_potential_stem = swp_1 + swp_2 * self.TM_day + swp_3 * self.RH_day + swp_4 * self.GR_day
@@ -393,6 +342,7 @@ class FruitFreshMatter(ParameterizedProcess):
             # ========================================================================================================================
             # WATER AND DRY MATTER CHANGES IN FRUIT COMPARTMENTS
             # ========================================================================================================================
+            # from fruit growth model in Léchaudel et al (2007)
 
             # -- rate of water inflow in the fruit from xylem and phloem (eq.4) :
             self.flux_xylem_phloem[fruiting] = ALf * (water_potential_stem - self.water_potential_fruit[fruiting])
@@ -406,9 +356,9 @@ class FruitFreshMatter(ParameterizedProcess):
             self.DM_flesh[fruiting] = self.DM_fleshpeel[fruiting] * e_fleshpeel2fleshDM
             self.W_flesh[fruiting] = self.W_fleshpeel[fruiting] * e_fleshpeel2fleshW
 
-            self.sucrose[fruiting] = mass_suc / FM_minus_stone
-            self.glucose[fruiting] = mass_glc / FM_minus_stone
-            self.fructose[fruiting] = mass_frc / FM_minus_stone
-            self.soluble_sugars[fruiting] = (mass_suc + mass_glc + mass_frc) / FM_minus_stone
-            self.starch[fruiting] = mass_sta / FM_minus_stone
-            self.organic_acids[fruiting] = (mass_mal + mass_cit) / FM_minus_stone
+            self.sucrose[fruiting] = self.mass_suc[fruiting] / FM_minus_stone
+            self.glucose[fruiting] = self.mass_glc[fruiting] / FM_minus_stone
+            self.fructose[fruiting] = self.mass_frc[fruiting] / FM_minus_stone
+            self.soluble_sugars[fruiting] = (self.mass_suc[fruiting] + self.mass_glc[fruiting] + self.mass_frc[fruiting]) / FM_minus_stone
+            self.starch[fruiting] = self.mass_sta[fruiting] / FM_minus_stone
+            self.organic_acids[fruiting] = (self.mass_mal[fruiting] + self.mass_cit[fruiting]) / FM_minus_stone
